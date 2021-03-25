a3c751f31559b5d725fc604b266f0492cb237c9e
AlmaPaes <paes@ciencias.unam.mx>
2021-03-24 22:00:29 -0600

    sendFile

--- myApp.js
+++ myApp.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
     res.send('Hello Express');
+});*/
+absolutePath = __dirname + '/views/index.html';
+
+app.get('/', (req, res) => {
+    res.sendFile(absolutePath);
 });
 
  module.exports = app;
