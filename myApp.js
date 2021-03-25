a4153618ec4b7dc24bf0c43bb6e16cf802ea593a
Alma Rosa Rosa <paes@ciencias.unam.mx>
2021-03-25 05:16:09 +0000

    add route public to app.use

--- myApp.js
+++ myApp.js
@@ -6,7 +6,7 @@ var app = express();
 });*/
 absoluteIndexPath = __dirname + '/views/index.html';
 
-app.use(express.static(__dirname + '/public'));
+app.use('/public',express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.sendFile(absoluteIndexPath);
